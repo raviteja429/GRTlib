b6290897178e5e2cfb4a354afd78ee2695d4a397
Raviteja <raviteja.g@nuncsystems.com>
2022-01-21 19:05:10 +0530

    Initial project setup

---
+++ GRTlib/GRTlib.h
@@ -0,0 +1,18 @@
+//
+//  GRTlib.h
+//  GRTlib
+//
+//  Created by venkat on 21/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GRTlib.
+FOUNDATION_EXPORT double GRTlibVersionNumber;
+
+//! Project version string for GRTlib.
+FOUNDATION_EXPORT const unsigned char GRTlibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRTlib/PublicHeader.h>
+
+
---
+++ GRTlibExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GRTlibExample
+//
+//  Created by venkat on 21/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
