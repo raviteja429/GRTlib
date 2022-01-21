5c6430d7854e2e061da9780408411fcb68d20071
Raviteja <raviteja.g@nuncsystems.com>
2022-01-21 19:35:10 +0530

    Added test cases

---
+++ GRTlib.swift
@@ -0,0 +1,22 @@
+//
+//  GRTlib.swift
+//  GRTlib
+//
+//  Created by venkat on 21/01/22.
+//
+
+import Foundation
+
+public final class GRTlib {
+
+    let name = "GRTlib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- GRTlibTests/GRTlibTests.swift
+++ GRTlibTests/GRTlibTests.swift
@@ -9,25 +9,20 @@ import XCTest
 @testable import GRTlib
 
 class GRTlibTests: XCTestCase {
+    
+    
+    var grtlib: GRTlib!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        grtlib = GRTlib()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(grtlib.add(a: 1, b: 1), 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSub() {
+        XCTAssertEqual(grtlib.sub(a: 3, b: 2), 1)
     }
 
 }
